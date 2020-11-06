dc288203bae2a7f08c3a3bc2da17601182c3a011
Cezary Mnich <cezary.mnich@student.vinci.be>
2020-11-06 10:11:29 +0100

    Add - table etudiants

--- projet.sql
+++ projet.sql
@@ -5,3 +5,10 @@ CREATE TABLE projet.formations (
 	nom VARCHAR(100) NOT NULL CHECK (nom<>''),
 	prenom VARCHAR(100) NOT NULL CHECK (prenom<>'')
 );
+CREATE TABLE projet.etudiants (
+	id_etudiant SERIAL PRIMARY KEY,
+	nom VARCHAR(50) NOT NULL CHECK (nom<>''),
+	prenom VARCHAR(50) NOT NULL CHECK (prenom<>''),
+	mot_de_passe VARCHAR(50) NOT NULL CHECK (mot_de_passe<>''),
+	code_bloc VARCHAR(10) REFERENCES projet.blocs (id_bloc) NOT NULL
+);
